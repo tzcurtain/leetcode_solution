c78f8fbc067ecf8d44825aa87c333fcbf4d55a46
Curtain <dw_zhoukechen@huya.com>
2019-08-22 16:47:29 +0800

    q41 finish

--- main.go
+++ main.go
@@ -3,7 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	candidates := []int{7, 3, 2}
-	target := 18
-	fmt.Println(combinationSum(candidates, target))
+	fmt.Print(firstMissingPositive([]int{1, 2, 3, 3}))
 }
