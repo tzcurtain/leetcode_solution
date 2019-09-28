854b32f2477e448ccf4cab93667b1335f320d9b3
TZCurtain <tzcurtain@foxmail.com>
2019-09-28 13:47:06 +0800

    q86 finish

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(maximalRectangle([]int{{}}))
+
+	ln := new(ListNode)
+	tmp := ln
+	ln.Val = 1
+	ln.Next = new(ListNode)
+	ln = ln.Next
+	ln.Val = 4
+	ln.Next = new(ListNode)
+	ln = ln.Next
+	ln.Val = 3
+	ln.Next = new(ListNode)
+	ln = ln.Next
+	ln.Val = 2
+	ln.Next = new(ListNode)
+	ln = ln.Next
+	ln.Val = 5
+	ln.Next = new(ListNode)
+	ln = ln.Next
+	ln.Val = 2
+
+	fmt.Println(partition(tmp, 3))
 }
