b9fb98724c1db4fc85d7d2bb878bd017abfd02bc
zhoukechen <zhoukechen@huya.com>
2019-10-17 09:48:28 +0800

    q102 finish

---
+++ 102.go
@@ -0,0 +1,25 @@
+package main
+
+// dfs
+
+func levelOrder(root *TreeNode) [][]int {
+	var res [][]int
+	levelOrderRecur(root, 0, res)
+	return res
+}
+
+func levelOrderRecur(root *TreeNode, level int, res [][]int) {
+	if root == nil {
+		return
+	}
+
+	if res == nil || len(res) <= level {
+		var tmp []int
+		res = append(res, tmp)
+	}
+
+	res[level] = append(res[level], root.Val)
+
+	levelOrderRecur(root.Left, level+1, res)
+	levelOrderRecur(root.Right, level+1, res)
+}
